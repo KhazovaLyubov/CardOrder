325c283827903a922f354c0e2e4218f079bf8489
Khazova Lyubov <lyubashilina@rambler.ru>
2020-12-11 11:50:28 +0400

    Added two tests

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -8,9 +9,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardOrderTest {
-    @Test
-    public void ValidNumberAndName(){
+    @BeforeEach
+    void setUp(){
         open("http://localhost:9999");
+    }
+
+    @Test
+    public void validNumberAndName(){
         $("[data-test-id=name] input").setValue("Хазова Любовь");
         $("[data-test-id=phone] input").setValue("+11111111111");
         $("[data-test-id=agreement]").click();
@@ -20,8 +25,7 @@ public class CardOrderTest {
     }
 
     @Test
-    public void NotValidName(){
-        open("http://localhost:9999");
+    public void notValidName(){
         $("[data-test-id=name] input").setValue("Khazova Lyubov");
         $("[data-test-id=phone] input").setValue("+11111111111");
         $("[data-test-id=agreement]").click();
@@ -31,8 +35,7 @@ public class CardOrderTest {
     }
 
     @Test
-    public void NotValidNumber(){
-        open("http://localhost:9999");
+    public void notValidNumber(){
         $("[data-test-id=name] input").setValue("Хазова Любовь");
         $("[data-test-id=phone] input").setValue("11111111111+");
         $("[data-test-id=agreement]").click();
@@ -42,8 +45,24 @@ public class CardOrderTest {
     }
 
     @Test
-    public void NotСonsentСheckbox(){
-        open("http://localhost:9999");
+    public void emptyFieldWithName(){
+        $("[data-test-id=name] input").setValue("Хазова Любовь");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    public void emptyFieldWithNumber(){
+        $("[data-test-id=phone] input").setValue("+11111111111");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void notСonsentСheckbox(){
         $("[data-test-id=name] input").setValue("Хазова Любовь");
         $("[data-test-id=phone] input").setValue("+11111111111");
         $("button").click();
